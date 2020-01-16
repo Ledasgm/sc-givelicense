f090e1321b4e9859162320edeb2d549e964599eb
Max Golf <mantas.gau@gmail.com>
2020-01-16 21:32:58 +0200

    small fix for /givelicense

--- cl-givelicense.lua
+++ cl-givelicense.lua
@@ -1,14 +1,13 @@
 RegisterCommand('givelicense', function()
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local job = xPlayer.job.name
     local closestPlayer, closestDistance = ESX.Game.GetclosestPlayer()
     
     if PlayerData.job and PlayerData.job.name == 'police' then
-        if PlayerData.job.grade_name == 'boss' then
+        --if PlayerData.job.grade_name == 'boss' then
             if closestPlayer ~= -1 and closestDistance <= 3.0 then
                 TriggerServerEvent('esx_license:addLicense', closestPlayer, 'weapon')
-        else
-            exports['mythic_notify']:SendAlert('error', 'Error')
-        end
-
     else
         exports['mythic_notify']:SendAlert('error', 'You are not a police officer')
     end
