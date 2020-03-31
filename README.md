1acde84f1a349159a8afbdd290906e388b7e2ac4
Maxgolf <52411193+Maxgolf@users.noreply.github.com>
2020-03-31 14:38:17 +0300

    Complete script rework & tested

--- README.md
+++ README.md
@@ -5,4 +5,19 @@ Give License command for police
 
 # Requirements
 - [ESX FRAMEWORK](https://github.com/ESX-Org/es_extended)
+
+# Optional
 - [Mythic Notify](https://github.com/mythicrp/mythic_notify)
+
+ Mythic Notify Notification system is optional. To use it uncomment:
+line: 35
+line: 40
+line 49
+
+ And if you do use it, don't forget to comment ESX notifications.
+line: 34
+line: 39
+line: 48
+
+# Join discord if you need help!
+[Discord](https://discord.gg/Zyq7kjw)
--- cl-givelicense.lua
+++ cl-givelicense.lua
@@ -1,18 +1,52 @@
-RegisterCommand('givelicense', function()
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local job = xPlayer.job.name
-    local closestPlayer, closestDistance = ESX.Game.GetclosestPlayer()
-    
-    if PlayerData.job and PlayerData.job.name == 'police' then
-        --if PlayerData.job.grade_name == 'boss' then
-            if closestPlayer ~= -1 and closestDistance <= 3.0 then
-                TriggerServerEvent('esx_license:addLicense', closestPlayer, 'weapon')
-    else
-        exports['mythic_notify']:SendAlert('error', 'You are not a police officer')
-    end
+-- WRITTEN BY MAX GOLF --------------------------
+-- Max Golf#0001  -------------------------------
+-- SUPPORT DISCORD: https://discord.gg/exZVrhB --
+-------------------------------------------------
 
-    else
-        exports['mythic_notify']:SendAlert('error', 'Higher police grade is required')
+
+ESX                           = nil
+local PlayerData              = {}
+
+Citizen.CreateThread(function ()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+        PlayerData = ESX.GetPlayerData()
     end
 end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+    PlayerData.job = job
+end)
+
+RegisterCommand('givelicense', function(source, args)
+    if PlayerData.job and PlayerData.job.name == 'police' then -- CHANGE TO YOUR JOB HERE 'police'
+        local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+        if closestPlayer ~= -1 and closestDistance <= 3.0 then
+                GiveLicenseToPlayer(closestPlayer) -- FUNCTION TO GIVE LICENSE
+            else
+                ESX.ShowNotification('There is no players nearby')
+                --exports['mythic_notify']:SendAlert('error', 'There is no players nearby')
+            end
+        
+            else
+                ESX.ShowNotification('You are not a police officer')
+                --exports['mythic_notify']:SendAlert('error', 'You are not a police officer')
+            end
+end, false)
+
+
+
+function GiveLicenseToPlayer(player) -- FUNCTION TO GIVE LICENSE
+
+    ESX.ShowNotification('You gave weapon license to ID: '.. GetPlayerServerId(player))
+    --exports['mythic_notify']:SendAlert('inform', 'You gave weapon license to ID: '.. GetPlayerServerId(player))
+    TriggerServerEvent('esx_license:addLicense', GetPlayerServerId(player), "weapon")
+
+end GetPlayerServerId(player)
